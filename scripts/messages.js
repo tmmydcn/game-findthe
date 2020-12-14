bcd0ea303d64cb78b5ea71ea4c6f13d112d497a6
tmmydcn <74939859+tmmydcn@users.noreply.github.com>
2020-12-14 02:55:20 +0100

    Update messages.js

--- scripts/messages.js
+++ scripts/messages.js
@@ -17,7 +17,7 @@ function searchMessage(key) {
 
 		/* EN */
 		case "en.title.object.default":					return "Object Default";
-		case "en.title.object.frog":					return "-Object Frog";
+		case "en.title.object.frog":					return "Object Frog";
 		case "en.title.object.pig":						return "Object Pig";
 		case "en.title.restart":						return "Restart";
 		case "en.text.restart":							return "Are you sure you want to restart?";
